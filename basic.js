75d473474dc7dd7de2fe807d45c1f1bdd92ea389
jming <jming@college.harvard.edu>
2014-04-25 01:58:47 -0400

    expert edition

--- basic.js
+++ basic.js
@@ -1,3 +1,5 @@
+var IS_EXPERT = true;
+
 
 // variables
 var ROW_MAX = 24;
@@ -8,8 +10,56 @@ var BOARDH_MAX = 500;
 
 var SQUARE = 20;
 
+// var NUM_GOALS = 12;
+var MAX_ACTIONS =150;
+
 // colors
 var LINECOLOR = "grey";
+
+BOARD = [
+	[0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+	[0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1],
+	[1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1],
+	[1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
+	[0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+	[0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+	[1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+	[1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+	[1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0],
+	[0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	[1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+	[1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+	[1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+	[0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0],
+	[1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0],
+	[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+	[0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0],
+	[1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+	[0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0],
+	[0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0],
+	[0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+	[1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+	[0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+]
+
+PATTERN_A = [[9,13], [10,10], [11,11], [11,12], [12,13], [13,9], [13,15], [14, 12], [15,9], [15, 11], [15, 15]];
+PATTERN_B = [[9,15], [10,11], [10,12], [10,14], [12,11], [13,11], [13,13], [14,15], [15,10], [15,14]];
+PATTERN_C = [[9,10], [9,12], [10,9], [10,13], [10,15], [11,9], [12,15], [13,12], [14,10], [14,13], [14,14]];
+PATTERN_D = [[9,9], [9,14], [11,10], [11,14], [11,15], [12,9], [13,10], [13,14], [14,9], [15,12]];
+PATTERN_E = [[9,11], [11,13], [12,10], [12,14], [14,11], [15,13]];
+
+var goals = []
+var colors = ['none', LINECOLOR, 'red', 'yellow', 'green', 'blue']
+var colors_distr = [0, 0, 14, 6, 6, 10];
+var actions = []
+var actions_categories = Array.apply(null, new Array(colors.length)).map(Number.prototype.valueOf, 0);
+var goal_order = []
+
+var pattern_order = [];
+var patterns = ['pattern_a', 'pattern_b', 'pattern_c', 'pattern_d', 'pattern_e'];
+var PATTERNS_CAPS = [PATTERN_A, PATTERN_B, PATTERN_C, PATTERN_D, PATTERN_E];
 
 // create board for tracking where things are
 
@@ -30,39 +80,11 @@ drawBoard();
 
 // determine blocks on the board
 
-BOARD = [
-	[0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1],
-	[0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1],
-	[1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1],
-	[1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
-	[0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-	[0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1],
-	[1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1],
-	[1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-	[1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0],
-	[0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-	[1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-	[1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
-	[1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-	[0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0],
-	[1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0],
-	[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1],
-	[0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0],
-	[1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1],
-	[0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0],
-	[0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0],
-	[0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1],
-	[1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
-	[0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-]
+
 
 // determine goals on the board
 
-var goals = []
-
-function defineGoals (num_goals, colors) {
+function defineGoals (colors) {
 
 	// loop through the colors
 	for (var c = 2; c < colors.length; c++) {
@@ -70,10 +92,10 @@ function defineGoals (num_goals, colors) {
 		// add on the number of goals
 		var j = 0;
 
-		while (j < num_goals) {
+		while (j < colors_distr[c]) {
 			var row = Math.floor(Math.random()*25);
 			var col = Math.floor(Math.random()*25);
-			if (BOARD[row][col] == 0) {
+			if (BOARD[row][col] == 0 && row != 12 && col != 12 && row != 0 && col != 0) {
 				BOARD[row][col] = c;
 				j++;
 			}
@@ -82,11 +104,8 @@ function defineGoals (num_goals, colors) {
 
 }
 
-var colors = ['none', LINECOLOR, 'red', 'yellow', 'green', 'blue']
-defineGoals(12, colors);
-
-// TODO: fix the board!
-
+defineGoals(colors);
+console.log('board', BOARD);
 
 function drawSquares () {
 
@@ -107,6 +126,28 @@ function drawSquares () {
 
 drawSquares();
 
+function drawHints() {
+	if (IS_EXPERT) {
+		for (var p = 0; p < patterns.length; p++) {
+
+			if (!(pattern_order.indexOf(p) > -1)) {
+				var pattern = PATTERNS_CAPS[p];
+
+				for (var i = 0; i < pattern.length; i++) {
+					var coords = pattern[i];
+					if (BOARD[coords[0]][coords[1]] == 1) {
+						// render the letter on the square
+						var letter = patterns[p].slice(-1).toUpperCase();
+						var letter_coords = getCoordinates(coords[0], coords[1]);
+						context.font = '16px Open Sans';
+						context.fillStyle = 'black';
+						context.fillText(letter, letter_coords.y+SQUARE*1/5, letter_coords.x+SQUARE*4/5);
+					}
+				}
+			}
+		}
+	}
+}
 
 // get coordinates
 
@@ -155,6 +196,8 @@ var draw = function () {
 	// blocks
 	drawSquares();
 
+	drawHints();
+
 	// player
 	if (playerReady) {
 		var coords = getCoordinates(player.row, player.col);
@@ -176,14 +219,156 @@ function checkIfBlocked (row, col) {
 	return (BOARD[row][col] != 1)
 }
 
-function reward (row, col) {
+function checkIfGoal (row, col) {
+
+	// in middle, end the game
+	if (row == 12 && col == 12) {
+
+		alert("Congrats! You have completed the game!");
+		endGame();
+
+	}
+
+	// check if colored square
 	var board_value = BOARD[row][col];
+
 	if (board_value != 0) {
+
+		// update the score
 		BOARD[row][col] = 0;
 		var color = colors[board_value];
 		var loc = $('#' + color + '_num');
 		loc.text(parseInt(loc.text()) + 1);
+
+		// log the goal_order
+		goal_order.push(board_value);
+
+		actions_categories[board_value] += 1;
 	}
+
+	// check if patterns obtained
+	checkPatterns();
+
+}
+
+function checkPatterns() {
+
+	var pattern = null;
+	var tiles = [];
+	var pattern_num = null;
+
+	// var colors = ['none', LINECOLOR, 'red', 'yellow', 'green', 'blue']
+
+	// one of each
+	if (actions_categories[2] > 0 
+		&& actions_categories[3] > 0 
+		&& actions_categories[4] > 0 
+		&& actions_categories[5] > 0) {
+		pattern = PATTERN_A;
+		tiles = [2, 3, 4, 5];
+		pattern_num = 0; 
+	}
+
+	// 4 of 2
+	if (actions_categories[2] > 3) {
+		pattern = PATTERN_B;
+		tiles = [2, 2, 2, 2];
+		pattern_num = 1;
+	}
+
+	if (actions_categories[2] > 1 
+		&& actions_categories[3] > 1) {
+		pattern = PATTERN_C;
+		tiles = [2, 2, 3, 3];
+		pattern_num = 2;
+	}
+
+	if (actions_categories[4] > 1
+		&& actions_categories[5] > 1) {
+		pattern = PATTERN_D;
+		tiles = [4, 4, 5, 5];
+		pattern_num = 3; 
+	}
+
+	if (actions_categories[5] > 1
+		&& actions_categories[2] > 1) {
+		pattern = PATTERN_E;
+		tiles = [2, 2, 5, 5];
+		pattern_num = 4;
+	}
+	
+
+	if (pattern 
+		&& tiles.length > 0 
+		&& !(pattern_order.indexOf(pattern_num) > -1)) {
+
+		alert('You have attained a combination!');
+		$( "#pattern" ).dialog();
+
+		// console.log('pattern', pattern);
+
+		// track that a pattern has been used
+		pattern_order.push(pattern_num);
+		
+		// color in the pattern used
+		$('#' + patterns[pattern_num]).append('<b>X</b>');
+		// var rects = $('#' + patterns[pattern_num] + ' svg rect');
+		// for (var h = 0; h < rects.length; h++) {
+		// 	rects[h].style.fill = LINECOLOR;
+		// }
+
+		// fix blocks in patterns
+		for (var i = 0; i < pattern.length; i++) {
+			var elt = pattern[i];
+			BOARD[elt[0]][elt[1]] = 0;
+		}
+
+		// fix number of colored tiles
+		for (var j = 0; j < tiles.length; j++) {
+			var color = colors[tiles[j]];
+
+			// display
+			var loc = $('#' + color + '_num');
+			loc.text(parseInt(loc.text()) - 1);
+
+			// stored value
+			actions_categories[tiles[j]] -= 1;
+		}
+	}
+	
+	
+
+}
+
+function addAction (key) {
+
+	// add to tracking actions
+	actions.push(key);
+
+	console.log(actions_categories);
+
+	var loc = $('#num_steps');
+	loc.text(parseInt(loc.text()) + 1);
+
+	// make sure not too many actions
+	if (actions.length == MAX_ACTIONS) {
+
+		alert("Oh no! You used too many steps. Please try again.");
+		endGame();
+
+	}
+}
+
+function endGame () {
+
+	context.fillStyle = LINECOLOR;
+	context.fillRect(0,0,520,520);
+	
+	// print actions
+	console.log('actions', actions);
+	console.log('goal_order', goal_order);
+	console.log('patterns', pattern_order);
+
 }
 
 // player input
@@ -225,10 +410,14 @@ function keyDownHandler(event) {
 			break;
 	}
 
+	// log the action
+	addAction(key);
+
 	// check if reward square
-	reward(player.row, player.col);
+	checkIfGoal(player.row, player.col);
 
 	draw();
+
 }
 
 // reset game
